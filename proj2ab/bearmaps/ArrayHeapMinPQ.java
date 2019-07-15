acdf7a8f3db94b784952c03e821edac9456ffb0f
patrickhsia <xjhsjtu2017@gmail.com>
2019-07-15 17:39:59 +0800

    完成proj2a

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -1,5 +1,6 @@
 package bearmaps;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 import java.util.Iterator;
 
@@ -12,27 +13,24 @@ import java.util.Iterator;
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
 
     private int size;                    //number of items on priority queue
-    private Node[] pq;                   //store items at indices 1 to n
-
+    private Node<T>[] pq;                   //store items at indices 1 to n
+    private HashMap<T,Integer> itemMap;    //为了contains的时间复杂度
     /**
      * constructor of ArrayHeapMinPQ
      */
     public ArrayHeapMinPQ(int initcapacity){
-        pq= (Node[]) new Object[initcapacity+1];             //泛型数组必须这样初始化,index从1开始
+        pq= new Node[initcapacity+1];             //泛型数组必须这样初始化,index从1开始
         size=0;
+        itemMap=new HashMap<>();
     }
     public ArrayHeapMinPQ(){
         this(1);
     }
 
-
-
-
-
     /**
      * the Node combining item and priority
      */
-    private class Node implements Comparable<Node>{
+    private class Node<T> implements Comparable<Node>{
         private T item;
         private double priority;
         Node(T item,double priority){
@@ -86,7 +84,21 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
      */
     @Override
     public void add(T item,double priority){
+        if (contains(item)) throw new IllegalArgumentException();
+        if (size==pq.length-1) resize(2*pq.length);        //第0项是没用的
+        Node x=new Node(item,priority);
+        pq[++size]=x;
+        itemMap.put(item,size);
+        swim(size);                                                 //将增加的项上浮
+        assert isMinHeap();
+    }
 
+    private void resize(int capacity){
+        Node[] temp=new Node[capacity];
+        for (int i=1;i<=size;i++){
+            temp[i]=pq[i];
+        }
+        pq=temp;
     }
 
     /**
@@ -106,7 +118,10 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
      */
     @Override
     public boolean contains(T item){
-        return false;
+        if (isempty()){
+            return false;
+        }
+        return itemMap.containsKey(item);
     }
 
     /**
@@ -125,7 +140,15 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
      */
     @Override
     public T removeSmallest(){
-        return null;
+        if (isempty()){
+            throw new NoSuchElementException();
+        }
+        T res=getSmallest();
+        exch(1,size--);                          //交换第一个和最后一个，并减少size
+        sink(1);                                //对第一个元素下沉
+        itemMap.remove(res);                        //将移除的元素从Hashmap中删除
+        pq[size+1]=null;
+        return res;
     }
 
     /**
@@ -149,17 +172,54 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
 
     @Override
     public void changePriority(T item,double priority){
-
+        if (isempty()) throw new IllegalArgumentException();
+        if (!contains(item)) throw new NoSuchElementException();
+        int index=itemMap.get(item);
+        double oldPriority=pq[index].getPriority();
+        pq[index].setPriority(priority);
+        if (oldPriority<priority){                       //优先级变高便下沉，否则变上浮
+            sink(index);
+        }else{
+            swim(index);
+        }
     }
 
     /**********************************************************
      * help method to restore the heap invariant
      **********************************************************/
+    //上浮
+    private void swim(int k){
+        while (k>1 && greater(k/2,k)){
+            exch(k/2,k);
+            k=k/2;
+        }
+    }
 
+    //下沉
+    private void sink(int k){
+        while (2*k<=size){
+            int j=2*k;
+            if (j<size && greater(j,j+1)) j+=1;          //选择两个子节点中的较小者
+            if (!greater(k,j)) break;                       //父节点比两个子节点都小，退出循环
+            exch(k,j);
+            k=j;
+        }
+    }
 
     /**********************************************************
      * Helper method for compare and swaps
      ***********************************************************/
+    private boolean greater(int i, int j){
+        return pq[i].compareTo(pq[j])>0;
+    }
+
+    private void exch(int i,int j){
+        Node temp=pq[i];
+        pq[i]=pq[j];
+        pq[j]=temp;
+        itemMap.put(pq[i].getItem(),i);                  //交换后HashMap做出相应改变
+        itemMap.put(pq[j].getItem(),j);
+    }
 
     private boolean isMinHeap(){
         return isMinHeap(1);
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,108 @@
 package bearmaps;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 public class ArrayHeapMinPQTest {
+    @Test
+    public void testadd(){
+        ArrayHeapMinPQ<Character> minheap=new ArrayHeapMinPQ<>();
+        minheap.add('g',4);
+        minheap.add('o',5);
+        minheap.add('d',6);
+        minheap.add('x',1);
+        minheap.add('j',2);
+        minheap.add('h',3);
+        assertTrue(minheap.size()==6);
+    }
+
+    @Test
+    public void testcontain(){
+        ArrayHeapMinPQ<Character> minheap=new ArrayHeapMinPQ<>();
+        minheap.add('g',4);
+        minheap.add('o',5);
+        minheap.add('d',6);
+        minheap.add('x',1);
+        minheap.add('j',2);
+        minheap.add('h',3);
+        assertTrue(minheap.contains('x'));
+        assertTrue(minheap.contains('d'));
+        assertFalse(minheap.contains('q'));
+    }
+
+    @Test
+    public void testremovesmallest(){
+        ArrayHeapMinPQ<Character> minheap=new ArrayHeapMinPQ<>();
+        minheap.add('g',3);
+        minheap.add('o',5);
+        minheap.add('d',6);
+        minheap.add('x',1);
+        minheap.add('j',2);
+        minheap.add('h',3);
+        assertTrue(minheap.removeSmallest()=='x');
+        minheap.removeSmallest();
+        assertTrue(minheap.removeSmallest()=='g');
+        assertTrue(minheap.removeSmallest()=='h');
+    }
+
+    @Test
+    public void testgetsmallest(){
+        ArrayHeapMinPQ<Character> minheap=new ArrayHeapMinPQ<>();
+        minheap.add('q',1);
+        minheap.add('r',2);
+        minheap.add('c',3);
+        minheap.add('x',1);
+        minheap.add('j',5);
+        minheap.add('h',6);
+        assertTrue(minheap.getSmallest()=='q');
+        assertFalse(minheap.getSmallest()=='x');
+    }
+    @Test
+    public void testchangepriority(){
+        ArrayHeapMinPQ<Character> minheap=new ArrayHeapMinPQ<>();
+        minheap.add('q',3);
+        minheap.add('r',2);
+        minheap.add('c',3);
+        minheap.add('x',1);
+        minheap.add('j',5);
+        minheap.add('h',6);
+        minheap.changePriority('q',1);
+        assertTrue(minheap.removeSmallest()=='x');
+        assertTrue(minheap.getSmallest()=='q');
+    }
+    public static void main(String[] args){
+        ArrayHeapMinPQ<Integer> minheap=new ArrayHeapMinPQ<>();
+        long start=System.currentTimeMillis();
+        for (int i=0;i<1000000;i++){
+            minheap.add(i,1000000-i);
+        }
+        long end=System.currentTimeMillis();
+        System.out.println("Total time elapsed:"+(end-start)/1000.0+"seconds.");
+        long start1=System.currentTimeMillis();
+        for (int i=0;i<1000;i++){
+            boolean t;
+            t=minheap.contains(i*300);
+        }
+        long end1=System.currentTimeMillis();
+        System.out.println("Total time elapsed:"+(end1-start1)/1000.0+"seconds");
+
+        /**
+         * compare to NaiveMinPQ
+         */
+        NaiveMinPQ<Integer> naivepq=new NaiveMinPQ<>();
+        long start2=System.currentTimeMillis();
+        for (int i=0;i<1000000;i++){
+            naivepq.add(i,1000000-i);
+        }
+        long end2=System.currentTimeMillis();
+        System.out.println("Naive total time elapsed:"+(end2-start2)/1000.0+"seconds.");
+        long start3=System.currentTimeMillis();
+        for (int i=0;i<1000;i++){
+            boolean t;
+            t=naivepq.contains(i*300);
+        }
+        long end3=System.currentTimeMillis();
+        System.out.println("Naive total time elapsed:"+(end3-start3)/1000.0+"seconds");
+    }
 }
